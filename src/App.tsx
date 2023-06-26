cbdcfd94ddf75abaec5903139c7f6ed7c1920e49
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2023-06-26 22:29:59 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { useLocation, Route, Routes } from 'react-router-dom'
+import Card from '@mui/joy/Card';
+import Box from '@mui/joy/Box';
+import Link from '@mui/joy/Link';
+import Stack from '@mui/joy/Stack';
+import Typography from '@mui/joy/Typography';
+import Chip from '@mui/joy/Chip';
 
-function App() {
+export const CurrentPath = () => {
+  const {pathname} = useLocation()
+  const [rendering, setRenderingState] = useState(false)
+
+  useEffect(() => {
+    setRenderingState(true)
+
+    let timer = setTimeout(() => setRenderingState(false), 600);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [pathname])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card sx={{ width: '500px', height: '80px', backgroundColor: rendering ? '#E6E6E6' : 'white', transition: 'background-color .5s' }}>
+      <Box display='flex' justifyContent='space-between'>
+        <Typography level="h2" fontSize="md" sx={{ mb: 0.5 }}>
+          CurrentPath
+        </Typography>
+        <Chip 
+          sx={{
+            opacity: !rendering ? "0" : "1",
+            transition: "all .5s",
+            visibility: !rendering ? "hidden" : "visible",
+          }}
+          color="danger"
+          size="sm">
+            Affected....
+          </Chip>
+      </Box>
+      <Typography>{pathname}</Typography>
+    </Card>
+  )
+}
+
+export const CurrenHash = () => {
+  const { hash } = useLocation()
+  const [rendering, setRenderingState] = useState(false)
+
+  useEffect(() => {
+    setRenderingState(true)
+
+    let timer = setTimeout(() => setRenderingState(false), 600);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [hash])
+
+  return (
+    <Card sx={{ width: '500px', height: '80px', backgroundColor: rendering ? '#E6E6E6' : 'white', transition: 'background-color .5s' }}>
+      <Box display='flex' justifyContent='space-between'>
+        <Typography level="h2" fontSize="md" sx={{ mb: 0.5 }}>
+          CurrentHash
+        </Typography>
+        <Chip sx={{
+          opacity: !rendering ? "0" : "1",
+          transition: "all .5s",
+          visibility: !rendering ? "hidden" : "visible",
+        }}color="success" size="sm">Rendering....</Chip>
+      </Box>
+      <Typography>{hash}</Typography>
+    </Card>
+  )
+}
+
+export const Links = () => {
+  return (
+    <Card orientation='vertical' sx={{ width: '500px', height: '80px' }}>
+      <Typography level="h2" fontSize="md" sx={{ mb: 0.5 }}>
+        Links
+      </Typography>
+      <Box  display='flex' gap={2}>
+      <Link href='#link1'>#link1</Link>
+      <Link href='#link2'>#link2</Link>
+      <Link href='#link3'>#link3</Link>
+      </Box>
+    </Card>
+  )
+}
+
+export const App = () => {
+  return (
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={4}
+      height='500px'
+      width='800px'
+      sx={{
+        backgroundColor: '#eeee',
+        margin: '0 auto'
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Links />} />
+      </Routes>
+      <CurrentPath />
+      <CurrenHash />
+    </Stack>
   );
 }
 
-export default App;
